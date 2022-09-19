9d6923cb45cc9c6a868d13f71511ca95e3bd72b9
ChenSC <136921225@qq.com>
2022-09-19 10:31:19 +0800

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
+>
+Java202201.iml,4\a\4ad037c9476b22722b2081828f1ecd267e87f178
+L
+.idea/codeStyles/Project.xml,6\e\6ecd6000a7b6f4a2884412ff19f74193ed089648
+T
+$.idea/codeStyles/codeStyleConfig.xml,d\d\dd7b3350c29c49497756c7f32c69a3bed18b1cba
--- src/com/java/exercise1.java
+++ src/com/java/exercise1.java
@@ -1,5 +1,10 @@
 package com.java;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 public class exercise1 {
     public static void main(String[] args) {
         int a = 100;
@@ -9,8 +14,40 @@ public class exercise1 {
         System.out.println(a);
     }
 }
+
 class Solution {
     public int flipLights(int n, int presses) {
 
+    }
+}
+
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int[] abc = new int[26];
+        int maxDifLen = -1;
+        for (int i = 0; i < s.length(); i++) {
+            abc[s.charAt(i) - 'a']++;
+            if (abc[s.charAt(i) - 'a'] > 1) {
+                maxDifLen = Math.max(maxDifLen, i - 1 - s.indexOf(s.charAt(i)));
+            }
+        }
+        return maxDifLen;
+    }
+}
+
+
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Integer[] tmp=new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i] , 0) + 1);
+            tmp[i]=nums[i];
+        }
+        Arrays.sort(tmp,(a, b)-> map.get(a).equals(map.get(b)) ?Integer.compare(b,a):map.get(a)- map.get(b));
+        for(int i = 0; i < nums.length; i++){
+            nums[i]=tmp[i];
+        }
+        return nums;
     }
 }
